1bc0f32f260ff70fbe556b986ba36cc3d0afe067
Bryden Frizzell <bryden.frizzell@gmail.com>
2022-12-14 14:44:30 +1300

    Moved version check to separate thread so it doesn't hold up initialization

--- Version.cpp
+++ Version.cpp
@@ -67,66 +67,73 @@ bool IsVersionNewer(std::string versionStr1, std::string versionStr2)
     return false;
 }
 
+DWORD WINAPI checkVersionThread(LPVOID param)
+{
+    DebugLog("Checking latest version...");
+
+    // get info of latest release on GitHub
+    WinHttpClient client(L"https://api.github.com/repos/BFrizzleFoShizzle/RE_Kenshi/releases/latest");
+    bool success = client.SendHttpRequest();
+    if (success)
+    {
+        // convert to regular string
+        std::wstring responseWstr = client.GetHttpResponse();
+        std::string responseStr(responseWstr.begin(), responseWstr.end());
+
+        // parse json
+        rapidjson::Document document;
+        document.Parse(responseStr.c_str());
+        // "tag_name" is version of release
+        // "vx.x.x"
+        latestVersionCache = document["tag_name"].GetString();
+        DebugLog("Latest public release: " + latestVersionCache);
+        if (Settings::GetSkippedVersion() == latestVersionCache)
+        {
+            DebugLog("Version " + latestVersionCache + " skipped.");
+            return 0;
+        }
+        // parse + compare version number strings
+        bool isRemoteNewer = IsVersionNewer(latestVersionCache, Version::GetCurrentVersion());
+        if (!isRemoteNewer)
+            DebugLog("We are up to date.");
+        else
+            DebugLog("Update is available from " + Version::GetCurrentVersion() + " to " + latestVersionCache);
+
+        // Ask if user wants to update if needed
+        if (!Version::IsCurrentVersion())
+        {
+            std::string msg = "A new version of RE_Kenshi (" + latestVersionCache + ") has been released.\nDo you want to update?";
+            int result = MessageBoxA(NULL, msg.c_str(), "RE_Kenshi update", MB_YESNO | MB_TOPMOST);
+            if (result == IDYES)
+            {
+                DebugLog("Opening browser...");
+                system("start https://www.nexusmods.com/kenshi/mods/847?tab=files");
+            }
+            if (result == IDNO)
+            {
+                std::string msg = "Do you want to skip RE_Kenshi release " + latestVersionCache + "?\n(disables notifications for this release)";
+                result = MessageBoxA(NULL, msg.c_str(), "RE_Kenshi update", MB_YESNO | MB_TOPMOST);
+                if (result == IDYES)
+                {
+                    Settings::SetSkippedVersion(latestVersionCache);
+                    MessageBoxA(NULL, "Version skipped!", "RE_Kenshi update", MB_OK | MB_TOPMOST);
+                }
+            }
+        }
+    }
+    else
+    {
+        DebugLog("Could not connect to update server. Update check failed.");
+    }
+    return 0;
+}
+
 void Version::Init()
 {
     if (Settings::GetCheckUpdates())
     {
-        DebugLog("Checking latest version...");
-
-        // get info of latest release on GitHub
-        WinHttpClient client(L"https://api.github.com/repos/BFrizzleFoShizzle/RE_Kenshi/releases/latest");
-        bool success = client.SendHttpRequest();
-        if (success)
-        {
-            // convert to regular string
-            std::wstring responseWstr = client.GetHttpResponse();
-            std::string responseStr(responseWstr.begin(), responseWstr.end());
-
-            // parse json
-            rapidjson::Document document;
-            document.Parse(responseStr.c_str());
-            // "tag_name" is version of release
-            // "vx.x.x"
-            latestVersionCache = document["tag_name"].GetString();
-            DebugLog("Latest public release: " + latestVersionCache);
-            if (Settings::GetSkippedVersion() == latestVersionCache)
-            {
-                DebugLog("Version " + latestVersionCache  + " skipped.");
-                return;
-            }
-            // parse + compare version number strings
-            bool isRemoteNewer = IsVersionNewer(latestVersionCache, GetCurrentVersion());
-            if (!isRemoteNewer)
-                DebugLog("We are up to date.");
-            else
-                DebugLog("Update is available from " + GetCurrentVersion() + " to " + latestVersionCache);
-
-            // Ask if user wants to update if needed
-            if (!IsCurrentVersion())
-            {
-                std::string msg = "A new version of RE_Kenshi (" + latestVersionCache + ") has been released.\nDo you want to update?";
-                int result = MessageBoxA(NULL, msg.c_str(), "RE_Kenshi update", MB_YESNO | MB_TOPMOST);
-                if (result == IDYES)
-                {
-                    DebugLog("Opening browser...");
-                    system("start https://www.nexusmods.com/kenshi/mods/847?tab=files");
-                }
-                if (result == IDNO)
-                {
-                    std::string msg = "Do you want to skip RE_Kenshi release " + latestVersionCache + "?\n(disables notifications for this release)";
-                    result = MessageBoxA(NULL, msg.c_str(), "RE_Kenshi update", MB_YESNO | MB_TOPMOST);
-                    if (result == IDYES)
-                    {
-                        Settings::SetSkippedVersion(latestVersionCache);
-                        MessageBoxA(NULL, "Version skipped!", "RE_Kenshi update", MB_OK |  MB_TOPMOST);
-                    }
-                }
-            }
-        }
-        else
-        {
-            DebugLog("Could not connect to update server. Update check failed.");
-        }
+        // moved to another thread as it causes other code to hang
+        CreateThread(NULL, 0, checkVersionThread, 0, 0, 0);
     }
 }
 
